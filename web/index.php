eaa3bfa0f70663c37a3f32e019972264237206c4
Stefan Hellings <hellingsstefan@gmail.com mailto:hellingsstefan@gmail.com>
2016-02-12 10:05:56 +0100

    improved routing in index page

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use Inventis\WebStandards\Group;
+use Inventis\WebStandards\NodeRouter;
 use Inventis\WebStandards\Twig\Extension\FileInclude;
 use Inventis\WebStandards\WebStandardsServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,17 +16,14 @@ $app['debug'] = true;
 // Web Standards
 $app->register(new WebStandardsServiceProvider());
 
-FileInclude:: setBaseDir(__DIR__ . '/docs');
-FileInclude:: setRenderer(new \Inventis\WebStandards\Renderers\DefaultRenderer());
-
 // Routing
 $app->match('/', function (Request $request) use ($app) {
     return $app['web-standards']
-        ->addGroup(__DIR__ . '/docs/home', Group::VISIBILITY_HIDDEN)
-        ->addGroup(__DIR__ . '/docs/standards', Group::VISIBILITY_DEV)
-        ->addGroup(__DIR__ . '/docs/pages'/*, Group::VISIBILITY_DEV*/)
-        ->addGroup(__DIR__ . '/docs/examples'/*, Group::VISIBILITY_DEV*/)
-        ->addGroup(__DIR__ . '/docs/partials'/*, Group::VISIBILITY_DEV*/)
+        ->addPath(__DIR__ . '/docs/home', NodeRouter::VISIBILITY_HIDDEN)
+        ->addPath(__DIR__ . '/docs/standards', NodeRouter::VISIBILITY_DEV)
+        ->addPath(__DIR__ . '/docs/pages'/*, NodeRouter::VISIBILITY_DEV*/)
+        ->addPath(__DIR__ . '/docs/examples'/*, NodeRouter::VISIBILITY_DEV*/)
+        ->addPath(__DIR__ . '/docs/partials'/*, NodeRouter::VISIBILITY_DEV*/)
         ->render($request->query->get('p', 'home/home.md'));
 });
 
