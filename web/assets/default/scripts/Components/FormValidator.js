f9c35cf65091d5db0c2e5541873450be3f68600b
Stijn Janssen <me@stijnjanssen.be>
2016-02-13 11:03:58 +0100

    first steps for the new validator

--- web/assets/default/scripts/Components/FormValidator.js
+++ web/assets/default/scripts/Components/FormValidator.js
@@ -1,12 +1,102 @@
-
 /* global require, define */
-require(['jquery'], function ($) {
+define(['jquery'], function ($) {
     "use strict";
 
-    // This require forces placeholder to be loaded after $ is available
-    require(['html5validator'], function() {
-        $('form').not('.no-js-validation').html5validator({
-            messages: formvalidator_translations
-        });
-    });
+    var FIELD_INVALID_CLASS = 'form__entry--invalid';
+
+    var Rules = {
+        required: function (value, options) {
+            return value.length > 0;
+        },
+        'max-length': function (value, options) {
+            return value.length < options.length;
+        }
+    };
+
+    var Validator = function (form, config) {
+        this.form = form;
+        this.config = config;
+        this.fields = [];
+
+        this.createFieldsBasedOnRules();
+
+        // add form submit listener
+    };
+
+    Validator.prototype.createFieldsBasedOnRules = function () {
+        var fields = this.config.fields,
+            field,
+            fieldName;
+
+        if (!fields) {
+            return false;
+        }
+
+        for (fieldName in fields) {
+            if (!fields.hasOwnProperty(fieldName)) {
+                continue;
+            }
+
+            field = this.createField(fieldName, fields[fieldName]);
+            if (field) {
+                this.fields.push(field);
+            }
+        }
+    };
+
+    Validator.prototype.createField = function (fieldName, rules) {
+        var field = this.form.querySelector("[name='" + fieldName + "']");
+
+        if (!field) {
+            return false;
+        }
+
+        return new Field(field, rules);
+    };
+
+    var Field = function (el, rules) {
+        this.el = el;
+        this.rules = rules;
+        this.failedRule;
+
+        this.el.addEventListener('blur', this.validate.bind(this));
+    };
+
+    Field.prototype.validate = function () {
+        this.el.parentNode.classList.remove(FIELD_INVALID_CLASS);
+
+        if (!this.isValid()) {
+            this.el.parentNode.classList.add(FIELD_INVALID_CLASS);
+            console.log(this.getValidationMessage());
+        }
+    };
+
+    Field.prototype.isValid = function () {
+        var i, length = this.rules.length, rule, options;
+
+        for (i=0; i<length; i++) {
+            rule = this.rules[i];
+            options = undefined;
+
+            if (typeof rule == 'object') {
+                options = rule[1];
+                rule = rule[0];
+            }
+
+            if (typeof Rules[rule] != 'undefined' && !Rules[rule](this.el.value, options)) {
+                this.failedRule = rule;
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    Field.prototype.getValidationMessage = function () {
+        // check for data-attr
+        // check for rule
+        // fallback on general translation
+    }
+
+    return Validator;
 });
--- web/assets/default/scripts/Main.js
+++ web/assets/default/scripts/Main.js
@@ -3,14 +3,23 @@ require(
     [
         'jquery',
         'Application/Manager',
+        'Application/Components/FormValidator',
         'Application/Components/Lightbox',
         'Application/Components/FlashMessages',
         'Application/Components/TextareaAutoGrow',
         'Application/Components/Placeholder',
-        'Application/Components/FormValidator',
         requireModule
     ],
-    function ($) {
+    function ($, Manager, FormValidator) {
         'use strict';
+
+        new FormValidator(
+            $('form')[0],
+            {
+                fields: {
+                    firstname: ['required', ['max-length', {length: 100}]]
+                }
+            }
+        );
     }
 );
