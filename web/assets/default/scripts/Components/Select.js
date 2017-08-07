9b255926eec2921025cd2002eaf6d27917b63e3d
__tom__ <tom@inventis.be>
2017-08-07 13:34:41 +0200

    Fix loop variable binding in Select

--- web/assets/default/scripts/Components/Select.js
+++ web/assets/default/scripts/Components/Select.js
@@ -3,20 +3,19 @@
     var component = document.querySelectorAll('[data-component="Select"]')
 
     for (var i = 0; i < component.length; i++) {
-
         var $select = component[i];
         var submitOnChange = $select.dataset.submitOnChange;
         var $select__label = $select.querySelector('.select__label');
         var $select__input = $select.querySelector('.select__input');
 
-        var updateValues = function() {
+        var updateValues = function($select__label, $select__input) {
             selectedValue = $select__input.options[$select__input.selectedIndex].value;
             var selectedText = $select__input.options[$select__input.selectedIndex].text;
             $select__label.innerHTML = selectedText;
         };
 
-        var onFieldChange = function () {
-            updateValues();
+        var onFieldChange = function (submitOnChange, $select__label, $select__input) {
+            updateValues($select__label, $select__input);
 
             if (submitOnChange == '1') {
                 $select__input.closest('form').submit();
@@ -24,8 +23,13 @@
         };
 
         // $select__input.on('change', onFieldChange);
-        $select__input.addEventListener('change', onFieldChange);
+        $select__input.addEventListener('change', onFieldChange.bind(
+            this,
+            submitOnChange,
+            $select__label,
+            $select__input
+        ));
 
-        updateValues();
+        updateValues($select__label, $select__input);
     }
 }());
