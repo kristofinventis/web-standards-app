1fd487e96becbadf6d1e5817af219c50a813df4a
Stefan Hellings <hellingsstefan@gmail.com>
2017-12-18 12:26:09 +0100

    If statement around osascript for pipeline

--- install.sh
+++ install.sh
@@ -28,18 +28,21 @@ composer install
 gulp build --theme styleguide
 gulp build
 
-osascript -e 'tell application "Terminal"
-    # Activate it.
-    activate
 
-    # Do command 1.
-    set firstCommand to "gulp browsersync"
-    do script firstCommand in window 0
+if [[ "$(which osascript)" != '' ]]; then
+    osascript -e 'tell application "Terminal"
+        # Activate it.
+        activate
 
-    # Open a new tab.
-    tell application "System Events" to tell process "Terminal" to keystroke "t" using command down
+        # Do command 1.
+        set firstCommand to "gulp browsersync"
+        do script firstCommand in window 0
 
-    # Do command 2.
-    set secondCommand to "cd web && PHP -S 127.0.0.1:8000"
-    do script secondCommand in window 1
-end tell'
+        # Open a new tab.
+        tell application "System Events" to tell process "Terminal" to keystroke "t" using command down
+
+        # Do command 2.
+        set secondCommand to "cd web && PHP -S 127.0.0.1:8000"
+        do script secondCommand in window 1
+    end tell'
+fi
